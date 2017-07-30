3d3d0d88de4d0514b254fc85f5aa86ba991a68fb
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-07-30 15:12:25 +0200

    initial stubs for w3cdom document conversion support

---
+++ src/main/java/ch/digitalfondue/jfiveparse/W3CDom.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2017 digitalfondue (info@digitalfondue.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ch.digitalfondue.jfiveparse;
+
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class W3CDom {
+
+    public static Document convert(ch.digitalfondue.jfiveparse.Document doc) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document d = builder.newDocument();
+            doc.traverse(new W3CDNodeVisitor(d));
+            return d;
+        } catch (ParserConfigurationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static class W3CDNodeVisitor implements NodesVisitor {
+
+        protected Document document;
+
+        public W3CDNodeVisitor(Document document) {
+            this.document = document;
+        }
+
+        @Override
+        public void start(Node node) {
+            //FIXME
+        }
+
+        @Override
+        public void end(Node node) {
+            //FIXME
+        }
+
+        @Override
+        public boolean complete() {
+            //FIXME
+            return false;
+        }
+    }
+}
