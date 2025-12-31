8a75dda46501ae75add9f2a51cff657f8da34290
Sylvain Jermini <sylvain.jermini@syjer.com>
2025-12-31 15:43:54 +0100

    update doc + examples

--- src/test/java/ch/digitalfondue/jfiveparse/example/LoadLobsters.java
+++ src/test/java/ch/digitalfondue/jfiveparse/example/LoadLobsters.java
@@ -8,11 +8,11 @@ import java.io.Reader;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 
-public class LoadHNTitle {
+public class LoadLobsters {
 
     public static void main(String[] args) throws IOException {
-        try (Reader reader = new InputStreamReader(URI.create("https://news.ycombinator.com/").toURL().openStream(), StandardCharsets.UTF_8)) {
-            NodeMatcher<Node> matcher = Selector.parseSelector("td.title > span.titleline > a");
+        try (Reader reader = new InputStreamReader(URI.create("https://lobste.rs/").toURL().openStream(), StandardCharsets.UTF_8)) {
+            NodeMatcher<Node> matcher = Selector.parseSelector("span[role=heading] a.u-url");
             JFiveParse.parse(reader).getAllNodesMatchingAsStream(matcher)
                     .map(Element.class::cast)
                     .forEach(a -> System.out.printf("%s [%s]\n", a.getTextContent(), a.getAttribute("href")));
