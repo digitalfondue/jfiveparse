d376c22031d5565dc1ee847cc56a3369f351bc9a
Sylvain Jermini <sylvain.jermini@syjer.com>
2026-08-04 22:23:11 +0200

    add commit info

---
+++ external-resources/commit.md
@@ -0,0 +1,2 @@
+https://github.com/html5lib/html5lib-tests.git 224991ec10db04f056a89eed8b0bd8695fd2950e
+https://github.com/web-platform-tests/wpt.git 73dff7c1179ee471bee7e005cdc8fc854ed76885
--- update_external_resources.java
+++ update_external_resources.java
@@ -5,9 +5,14 @@
 import com.google.common.io.MoreFiles;
 import org.apache.commons.io.FileUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 public class update_external_resources {
 
@@ -18,9 +23,20 @@ public class update_external_resources {
             tempDir = Files.createTempDirectory("jbang-git-clone-");
             System.out.println("Created temporary directory: " + tempDir);
 
+            List<String> commitLogs = new ArrayList<>();
+
             // 2. Clone repositories
-            cloneRepo(tempDir, "https://github.com/html5lib/html5lib-tests.git");
-            cloneRepo(tempDir, "https://github.com/web-platform-tests/wpt.git");
+            String repo1Url = "https://github.com/html5lib/html5lib-tests.git";
+            Path repo1Path = tempDir.resolve("html5lib-tests");
+            cloneRepo(tempDir, repo1Url);
+            String repo1Hash = getCommitHash(repo1Path);
+            commitLogs.add(repo1Url + " " + repo1Hash);
+
+            String repo2Url = "https://github.com/web-platform-tests/wpt.git";
+            Path repo2Path = tempDir.resolve("wpt");
+            cloneRepo(tempDir, repo2Url);
+            String repo2Hash = getCommitHash(repo2Path);
+            commitLogs.add(repo2Url + " " + repo2Hash);
 
             MoreFiles.deleteRecursively(Path.of("external-resources"));
 
@@ -33,6 +49,14 @@ public class update_external_resources {
             Path sourceWptResources = tempDir.resolve("wpt").resolve("html").resolve("syntax").resolve("parsing").resolve("resources");
             Path targetWptResources = Path.of("external-resources", "wpt", "html", "syntax", "parsing", "resources");
             copyDirectoryIfExists(sourceWptResources, targetWptResources);
+
+            // 5. Write external-resources/commit.md
+            Path externalResourcesDir = Path.of("external-resources");
+            Files.createDirectories(externalResourcesDir);
+            Path commitFile = externalResourcesDir.resolve("commit.md");
+            System.out.println("\nWriting commit info to " + commitFile.toAbsolutePath() + " ...");
+            Files.write(commitFile, commitLogs, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            System.out.println("Successfully wrote commit.md.");
 
         } catch (Exception e) {
             System.err.println("An error occurred during execution: " + e.getMessage());
@@ -71,6 +95,21 @@ public class update_external_resources {
             System.out.println("Successfully copied.");
         } else {
             System.err.println("Source path does not exist: " + source);
+        }
+    }
+
+    private static String getCommitHash(Path repoDir) throws IOException, InterruptedException {
+        ProcessBuilder pb = new ProcessBuilder("git", "rev-parse", "HEAD");
+        pb.directory(repoDir.toFile());
+        Process process = pb.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String hash = reader.readLine();
+            int exitCode = process.waitFor();
+            if (exitCode != 0 || hash == null) {
+                throw new RuntimeException("Failed to get commit hash for " + repoDir);
+            }
+            return hash.trim();
         }
     }
 }
