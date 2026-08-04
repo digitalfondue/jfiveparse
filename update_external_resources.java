e213cf4b3da9b75b848d8c8b98b0575995ca4e8e
Sylvain Jermini <sylvain.jermini@syjer.com>
2026-08-04 21:41:45 +0200

    wip script for copying test files

--- update_external_resources.java
+++ update_external_resources.java
@@ -1,49 +1,44 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS com.google.guava:guava:33.4.0-jre
+//DEPS commons-io:commons-io:2.18.0
 
 import com.google.common.io.MoreFiles;
-import com.google.common.io.RecursiveDeleteOption;
+import org.apache.commons.io.FileUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class update_external_resources {
 
     public static void main(String[] args) {
         Path tempDir = null;
         try {
+            // 1. Create a temporary directory
             tempDir = Files.createTempDirectory("jbang-git-clone-");
             System.out.println("Created temporary directory: " + tempDir);
 
-            List<String> repos = List.of(
-                    "https://github.com/html5lib/html5lib-tests.git",
-                    "https://github.com/web-platform-tests/wpt.git"
-            );
+            // 2. Clone repositories
+            cloneRepo(tempDir, "https://github.com/html5lib/html5lib-tests.git");
+            cloneRepo(tempDir, "https://github.com/web-platform-tests/wpt.git");
 
-            for (String repoUrl : repos) {
-                System.out.println("\nCloning: " + repoUrl + " ...");
-                ProcessBuilder pb = new ProcessBuilder("git", "clone", repoUrl);
-                pb.directory(tempDir.toFile());
-                pb.inheritIO();
-                Process process = pb.start();
-                int exitCode = process.waitFor();
-                if (exitCode != 0) {
-                    String msg = "Failed to clone " + repoUrl + " (Exit code: " + exitCode + ")";
-                    System.err.println(msg);
-                    throw new IllegalStateException(msg);
-                } else {
-                    System.out.println("Successfully cloned " + repoUrl);
-                }
-            }
+            MoreFiles.deleteRecursively(Path.of("external-resources2"));
 
-            // TODO: git checkout to specific commit
-            // TODO: copy files / directories that we need for our test
+            // 3. Copy html5lib-tests/tokenizer
+            Path sourceTokenizer = tempDir.resolve("html5lib-tests").resolve("tokenizer");
+            Path targetTokenizer = Path.of("external-resources2", "html5lib-tests", "tokenizer");
+            copyDirectoryIfExists(sourceTokenizer, targetTokenizer);
+
+            // 4. Copy wpt/html/syntax/parsing/resources
+            Path sourceWptResources = tempDir.resolve("wpt").resolve("html").resolve("syntax").resolve("parsing").resolve("resources");
+            Path targetWptResources = Path.of("external-resources2", "wpt", "html", "syntax", "parsing", "resources");
+            copyDirectoryIfExists(sourceWptResources, targetWptResources);
 
         } catch (Exception e) {
             System.err.println("An error occurred during execution: " + e.getMessage());
+            e.printStackTrace();
         } finally {
+            // 5. Clean up temporary directory using Guava
             if (tempDir != null && Files.exists(tempDir)) {
                 System.out.println("\nCleaning up temporary directory: " + tempDir);
                 try {
@@ -53,6 +48,29 @@ public class update_external_resources {
                     System.err.println("Failed to delete temp directory: " + e.getMessage());
                 }
             }
+        }
+    }
+
+    private static void cloneRepo(Path workDir, String repoUrl) throws IOException, InterruptedException {
+        System.out.println("\nCloning: " + repoUrl + " ...");
+        ProcessBuilder pb = new ProcessBuilder("git", "clone", "--depth", "1", repoUrl);
+        pb.directory(workDir.toFile());
+        pb.inheritIO();
+
+        Process process = pb.start();
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new RuntimeException("Failed to clone " + repoUrl + " (Exit code: " + exitCode + ")");
+        }
+    }
+
+    private static void copyDirectoryIfExists(Path source, Path target) throws IOException {
+        System.out.println("\nCopying directory: " + source + " -> " + target.toAbsolutePath() + " ...");
+        if (Files.exists(source)) {
+            FileUtils.copyDirectory(source.toFile(), target.toFile());
+            System.out.println("Successfully copied.");
+        } else {
+            System.err.println("Source path does not exist: " + source);
         }
     }
 }
