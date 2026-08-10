07d7c347b2f654da8b6db5c372f29fea70fc8d8e
Sylvain Jermini <sylvain.jermini@syjer.com>
2026-08-10 21:32:51 +0200

    muh

--- src/main/java/ch/digitalfondue/jfiveparse/ProcessedInputStream2.java
+++ src/main/java/ch/digitalfondue/jfiveparse/ProcessedInputStream2.java
@@ -16,7 +16,7 @@ final class ProcessedInputStream2 {
     private final Reader reader;
     private final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024 * Character.BYTES);
     private final CharBuffer charBuffer = byteBuffer.asCharBuffer();
-    //private final LongBuffer longBuffer = byteBuffer.asLongBuffer();
+    private final LongBuffer longBuffer = byteBuffer.asLongBuffer();
     //private final CharBuffer charBuffer = CharBuffer.allocate(1024);
 
     ProcessedInputStream2(Reader reader) {
@@ -34,7 +34,26 @@ final class ProcessedInputStream2 {
             // the buffer, in this case, we replace it with LF, and at the next fill, if the first
             // char is a LF, we advance position by 1 thus skipping it
             charBuffer.position(0);
-            charBuffer.limit(Math.max(count, 0));
+
+            var limit = Math.max(count, 0);
+            /*
+            for (int i = 0; i < limit/4; i++) {
+                // TODO: here can do SWAR for CR LF search
+                //       and rewrite
+                long packed = longBuffer.get(i);
+                char c1 = (char) (packed >>> 48);
+                char c2 = (char) (packed >>> 32);
+                char c3 = (char) (packed >>> 16);
+                char c4 = (char) (packed);
+                String unpackedString = new String(new char[]{c1, c2, c3, c4});
+                System.out.println(unpackedString);
+            }
+            for (int i = limit - (limit % 4); i < limit; i++) {
+                System.out.println(charBuffer.get(i));
+            }
+            */
+
+            charBuffer.limit(limit);
             return count < 1;
         } catch (IOException e) {
             throw new ParserException(e);
